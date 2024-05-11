900ff3b69951e75cb5fbd5a9a0459585e091109c
muhammad fachrurrozi <ozjibuwat@gmail.com>
2024-05-11 08:28:53 +0700

    progress add button

--- 06-react-forms/src/App.js
+++ 06-react-forms/src/App.js
@@ -5,7 +5,9 @@ function App() {
     lastName:"",
     email:"",
     dob:"",
-    gender:""
+    gender:"",
+    prompt1:"",
+    answer1:""
   })
   const handleInput = (e) =>{
     const {name, value} = e.target;
@@ -13,12 +15,15 @@ function App() {
       ...userInfo,[name]:value
     })
   }
+  const handleAddPrompt=()=>{
+    
+  }
   console.log(userInfo);
   return (
     
     <>
       <h1 className="text-3xl text-center my-4 py-2">06-react-forms</h1>
-      <form className="w-5/6 max-w-md mx-auto">
+      <form className="w-5/6 max-w-md mx-auto pt-4 pb-10">
         <fieldset className="flex flex-col border py-1 px-4">
           <legend className="text-2xl font-semibold text-gray-500">About You</legend>
           <div>
@@ -72,9 +77,27 @@ function App() {
         </fieldset>
         <fieldset className="flex flex-col border py-1 px-4">
           <legend className="text-2xl font-semibold text-gray-500">
-            Prompt
+            Prompts
           </legend>
-          <div></div>
+          <div className="flex flex-col">
+            <label className="text-3xl font-semibold">select a Prompt</label>
+            <select className="w-3/5 border rounded text-lg leading-tight py-3 px-2 mt-4 focus:outline-indigo-200 mb-4"
+            id="prompt1" name="prompt1" onChange={handleInput}>
+              <option value={"Dating me is like..."}>Dating me is like...</option>
+              <option value={"Fact about me that surprises people"}>Fact about me that surprises people:</option>
+              <option value={"I want someone who..."}>I want someone who...</option>
+              <option value={"I spend of most money on :"}>I spend of most money on :</option>
+              <option value={"the most spontanoes thing i've done:"}>the most spontanoes thing i've done:</option>
+              <option value={"We're the same type of weird if..."}>We're the same type of weird if...</option>
+            </select>
+            <textarea className="border border-dashed mb-4 py-3 px-2" id="answer1" name="answer1" rows={5} placeholder="let your true colour shine through" onChange={handleInput}/>
+          </div>
+          <div className="w-full flex justify-center">
+            <button className="border bg-indigo-400 py-1 px-2 rounded-lg text-white font-bold text-xl"
+            type="button"
+            onClick={handleAddPrompt}
+            >Add Prompt</button>
+          </div>
         </fieldset>
       </form>
     </>
