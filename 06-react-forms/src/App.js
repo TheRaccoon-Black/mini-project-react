f2eb1fbf28e353cd1f5517e08f7755fd58edb4c9
muhammad fachrurrozi <ozjibuwat@gmail.com>
2024-05-11 08:07:13 +0700

    add promptss

--- 06-react-forms/src/App.js
+++ 06-react-forms/src/App.js
@@ -1,9 +1,21 @@
-
+import {useState} from "react";
 function App() {
-  const handleInput = () =>{
-
+  const [userInfo,setUserInfo] = useState({
+    firstName:"",
+    lastName:"",
+    email:"",
+    dob:"",
+    gender:""
+  })
+  const handleInput = (e) =>{
+    const {name, value} = e.target;
+    setUserInfo({
+      ...userInfo,[name]:value
+    })
   }
+  console.log(userInfo);
   return (
+    
     <>
       <h1 className="text-3xl text-center my-4 py-2">06-react-forms</h1>
       <form className="w-5/6 max-w-md mx-auto">
@@ -51,12 +63,18 @@ function App() {
            <label className="text-3xl font-semibold">What's your gender</label>
            <select 
            className="w-3/5 border rounded text-lg leading-tight py-3 px-2 mt-4 focus:outline-indigo-200 mb-4"
-            id="gender" name="gender">
+            id="gender" name="gender" onChange={handleInput}>
             <option>Gender</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
            </select>
            </div>
+        </fieldset>
+        <fieldset className="flex flex-col border py-1 px-4">
+          <legend className="text-2xl font-semibold text-gray-500">
+            Prompt
+          </legend>
+          <div></div>
         </fieldset>
       </form>
     </>
